3dbcacf57d9cfa71c16bfb033c10f2e803c84b1f
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-06 16:14:28 +0530

    node event loof discussing

---
+++ event-loop.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+setTimeout(() => {
+  console.log("setTimeout");
+}, 0);
+
+setImmediate(() => {
+  console.log("setImmediate");
+});
+fs.readFile("test-file.txt", () => {
+  console.log("I/O finished");
+  console.log("-------------------");
+  setTimeout(() => {
+    console.log("setTimeout 2");
+  }, 0);
+  setTimeout(() => {
+    console.log("setTimeout 3");
+  }, 3000);
+
+  setImmediate(() => {
+    console.log("setImmediate 2");
+  });
+});
+console.log("Hello from the top-level code");
