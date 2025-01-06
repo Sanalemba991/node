1e83f9cd7fd2e749802186250c12e80c75622d6c
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-06 17:04:11 +0530

    making good

--- event-loop.js
+++ event-loop.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const crypto = require("crypto");
 const start = Date.now();
-process.env.UV_THREADPOOL_SIZE = 1;
+
+// (Optional) Set the thread pool size if needed for async crypto operations
+process.env.UV_THREADPOOL_SIZE = 2;
 
 console.log("Hello from the top-level code");
 
@@ -30,18 +32,25 @@ fs.readFile("test-file.txt", () => {
   });
 
   process.nextTick(() => {
+    // Correct usage of pbkdf2 (asynchronous) with a callback
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", (err, derivedKey) => {
+      if (err) throw err;
+      console.log(Date.now() - start, "Password 1 encrypted");
+    });
 
-    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-      console.log(Date.now() - start, "Password  encrypted");
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", (err, derivedKey) => {
+      if (err) throw err;
+      console.log(Date.now() - start, "Password 2 encrypted");
     });
-    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-      console.log(Date.now() - start, "Password  encrypted");
+
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", (err, derivedKey) => {
+      if (err) throw err;
+      console.log(Date.now() - start, "Password 3 encrypted");
     });
-    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-      console.log(Date.now() - start, "Password  encrypted");
-    });
-    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
-      console.log(Date.now() - start, "Password  encrypted");
+
+    crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", (err, derivedKey) => {
+      if (err) throw err;
+      console.log(Date.now() - start, "Password 4 encrypted");
     });
   });
 });
