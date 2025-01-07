14f3c3e640d33b969d07d94164b0aebce60ff48b
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2025-01-07 16:25:38 +0530

    add dir name json

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side", app: "Natours" });
-});
+// app.get("/", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: "Hello from the server side", app: "Natours" });
+// });
 
-app.post("/", (req, res) => {
-  res.send("You can post to this endpoint...");
-});
+// app.post("/", (req, res) => {
+//   res.send("You can post to this endpoint...");
+// });
+
+app.get('/api/tours', (req, res) => {
+    res.status(200).json({
+
+        status: 'success',
+
+    
+
+    })
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
